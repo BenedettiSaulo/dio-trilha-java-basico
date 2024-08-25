0c46d4e52531b37bbdaba8a0bc5af16c0dc8143b
Saulo <saulo.benedetti@hotmail.com>
2024-08-25 18:34:03 -0300

    Concluindo desafio de controle de fluxo com Java

---
+++ desafios/controle-fluxo/src/application/Contador.java
@@ -0,0 +1,45 @@
+package application;
+
+import java.util.Scanner;
+
+import exceptions.ParametrosInvalidosException;
+
+public class Contador {
+
+	public static void main(String[] args) {
+
+		Scanner terminal = new Scanner(System.in);
+
+		System.out.println("Digite o primeiro parâmetro");
+		int parametroUm = terminal.nextInt();
+
+		System.out.println("Digite o segundo parâmetro");
+		int parametroDois = terminal.nextInt();
+
+		try {
+
+			contar(parametroUm, parametroDois);
+
+		} catch (ParametrosInvalidosException exception) {
+
+			System.out.println(exception.getMessage());
+		}
+
+		terminal.close();
+	}
+
+	static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+
+		if (parametroUm > parametroDois) {
+
+			throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
+		}
+
+		int contagem = parametroDois - parametroUm;
+
+		for (int cont = 1; cont <= contagem; cont++) {
+
+			System.out.printf("Imprimindo o número %d\n", cont);
+		}
+	}
+}
